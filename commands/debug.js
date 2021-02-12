f9f691ecb469933658fe79b0a2c656426cd070f2
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-12 11:51:42 -0300

    Update debug.js

--- commands/debug.js
+++ commands/debug.js
@@ -1,18 +1,23 @@
-const { ptBR } = require("date-fns/locale");
-const { format, parseISO } = require("date-fns");
+// const { ptBR } = require("date-fns/locale");
+// const { format, parseISO } = require("date-fns");
+const moment = require("moment");
 
-function formatDate(data) {
-  let fdata = [...data];
-  return `${fdata[8]}${fdata[9]}-${fdata[5]}${fdata[6]}-${fdata[0]}${fdata[1]}${fdata[2]}${fdata[3]}`; // Ano Mes Dia
-}
+// function formatDate(data) {
+//   let fdata = [...data];
+//   return `${fdata[8]}${fdata[9]}-${fdata[5]}${fdata[6]}-${fdata[0]}${fdata[1]}${fdata[2]}${fdata[3]}`; // Ano Mes Dia
+// }
 
 module.exports = async (message, args) => {
   switch (args[0]) {
     case "date":
-      var result = formatDate(args[1]);
-      console.log(result);
-      var fresult = format(parseISO(result), "MMMM d", { locale: ptBR });
-      console.log(fresult);
+      moment.locale("pt-br");
+      console.log(moment(new Date("10 15")).format("d [de] MMMM"));
+      
+
+      // var result = formatDate(args[1]);
+      // console.log(result);
+      // var fresult = format(parseISO(result), "MMMM d", { locale: ptBR });
+      // console.log(fresult);
     default:
       console.log(`D|>|Debug: ${args}`);
   }
