7e7f3f885b14967d0735e1ecc0e2805ac123145f
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-12 06:54:59 -0300

    Update debug.js

--- commands/debug.js
+++ commands/debug.js
@@ -4,9 +4,9 @@ module.exports = async (message, args) => {
   switch (args[0]) {
     case "date":
       console.log(`D|>|Date: ${args[1]}`)
-      const result = processDate(args[1], "monthAndDate"); // ~~"MMMM d" or~~ "monthAndDate"
+      const result = processDate(args[1]); // "MMMM d" "monthAndDate"
       console.log(`D|>|Result: ${result}`);
-      // message.channel.send(result);
+      message.channel.send(result);
       break;
     default:
       console.log(`D|>|Debug: ${args}`);
