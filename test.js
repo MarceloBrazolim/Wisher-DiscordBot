61cf2fe121b7b377b24aab700fcd646d7eac4126
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-12 07:34:48 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 const { ptBR } = require("date-fns/locale");
 const { format, parseISO } = require("date-fns");
 
-const DateFnsAdapter = require("@date-io/date-fns");
+// const DateFnsAdapter = require("@date-io/date-fns");
 
-const dateFns = new DateFnsAdapter({ locale: ptBR });
+// const dateFns = new DateFnsAdapter({ locale: ptBR });
 // console.log(dateFns);
 
 var a = "2014-02-11T11:30:30";
 console.log(a);
-const initialDateFnsDate = dateFns.date(a);
-console.log(initialDateFnsDate);
 
-console.log(dateFns.format(initialDateFnsDate, "fullDateTime24h")); // "2018, October 30th 11:44"
+var b = format(parseISO(a), /*typeFormat*/ "MMMM d", { locale: ptBR });
+console.log(b);
+// const initialDateFnsDate = dateFns.date(a);
+// console.log(initialDateFnsDate);
+
+// console.log(dateFns.format(initialDateFnsDate, "fullDateTime24h")); // "2018, October 30th 11:44"
