369d27c1d303716ccd8d45f11d0081018d3baf58
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-12 18:21:53 -0300

    5k+

--- commands/setBirthday.js
+++ commands/setBirthday.js
@@ -10,13 +10,30 @@ module.exports = async (message, args, uID, client) => {
   var ID = client.users.cache.get("805035898990755850");
   var confirmationEmbed = new Discord.MessageEmbed()
     .setColor("#831fde")
-    .setTitle("Confirmação")
+    .setTitle("Blz! A data de aniversário tá correta?")
     .setAuthor("Wisher", ID.displayAvatarURL({ dynamic: true }))
-    .addField("Blz! A data de aniversário tá correta?", date);
+    .setDescription(date);
 
   let msgEmbed = await message.channel.send(confirmationEmbed);
   var reactions = ["❌", "🔸", "✅"];
-  await addReaction(msgEmbed, reactions)
+  await addReaction(msgEmbed, reactions);
+
+  const handleReactions = (reaction, user, add) => {
+    if (user.id === "805035898990755850") return;
+
+    const emoji = reaction._emoji.name;
+    const { guild } = reaction.message;
+
+    const member = guild.members.chache.find((member) => member.id === user.id);
+
+    if (add) {
+
+    }
+  };
+
+  client.on("messageReactionAdd", (reaction, user) => {
+    handleReactions(reaction, user, true);
+  });
 
   // Inserts into DB
   // await update(date, uID);
