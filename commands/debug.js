89b316401ba56d0999c832261bc942399dc729cc
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-12 12:14:52 -0300

    IT WORKS!!!!!!

--- commands/debug.js
+++ commands/debug.js
@@ -7,19 +7,19 @@ const moment = require("moment");
 //   return `${fdata[8]}${fdata[9]}-${fdata[5]}${fdata[6]}-${fdata[0]}${fdata[1]}${fdata[2]}${fdata[3]}`; // Ano Mes Dia
 // }
 
-// module.exports = async (message, args) => {
-  var args = "date";
-  switch (args) {
+module.exports = async (message, args) => {
+  switch (args[0]) {
     case "date":
       moment.locale("pt-br");
-      console.log(moment(new Date("10 15")).format("d [de] MMMM"));
-      
+      var result = moment(new Date(args[1])).format("DD [de] MMMM");
+      console.log(`D|>|Debug: ${result}`);
+      message.channel.send(result);
 
-      // var result = formatDate(args[1]);
-      // console.log(result);
-      // var fresult = format(parseISO(result), "MMMM d", { locale: ptBR });
-      // console.log(fresult);
+    // var result = formatDate(args[1]);
+    // console.log(result);
+    // var fresult = format(parseISO(result), "MMMM d", { locale: ptBR });
+    // console.log(fresult);
     default:
       console.log(`D|>|Debug: ${args}`);
   }
-// };
+};
