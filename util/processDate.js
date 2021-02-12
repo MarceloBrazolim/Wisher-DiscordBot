2807c24fcc11a4e13c123e4c77c076850a40ec76
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-12 06:41:28 -0300

    Update processDate.js

--- util/processDate.js
+++ util/processDate.js
@@ -1,12 +1,12 @@
 const { ptBR } = require("date-fns/locale");
-// const format = require("date-fns/format");
-const DateFnsAdapter = require("@date-io/date-fns");
+const format = require("date-fns/format");
+// const DateFnsAdapter = require("@date-io/date-fns");
 
 module.exports = async (date, typeFormat) => {
-  const dateFns = new DateFnsAdapter({ locale: ptBR });
-  const initialDate = dateFns.date(date); // Ex: 2018-10-28T11:44:00.000Z .toLowerCase()
+  // const dateFns = new DateFnsAdapter({ locale: ptBR });
+  // const initialDate = dateFns.date(date); // Ex: 2018-10-28T11:44:00.000Z .toLowerCase()
 
-  return dateFns.format(initialDate, typeFormat);
+  // return dateFns.format(initialDate, typeFormat);
 
-  // return format(date, typeFormat, { locale: ptBR });
+  return format(date, /*typeFormat*/"MMMM d", { locale: ptBR });
 };
