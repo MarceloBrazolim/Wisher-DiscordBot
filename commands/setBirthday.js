31a4715fc73e5ea1443605793ee63150740ef3ae
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-14 02:40:15 -0300

    v1.4.9

--- commands/setBirthday.js
+++ commands/setBirthday.js
@@ -3,19 +3,28 @@ const { prefix } = require("../config.json");
 const addReaction = require("../util/addReaction");
 //
 const confirmYes = new Discord.MessageEmbed();
-
 const confirmNo = new Discord.MessageEmbed();
-
 const confirmationEmbed = new Discord.MessageEmbed();
 
-module.exports = async (message, uID, date) => {
-  confirmationEmbed
+module.exports = async (message, u, date) => {
+  confirmationEmbed = new Discord.MessageEmbed()
     .setColor("#831fde")
     .setTitle("Blz! A data de aniversário está certa?")
     .setDescription(`${date}.`);
-  var msgEmbed = await message.channel.send(confirmationEmbed);
+  const message.channel.fetch().then((wMessage) => { await message.channel.send(confirmationEmbed);}
   const reactions = ["❌", "🔸", "✅"];
   await addReaction(msgEmbed, reactions);
+
+  confirmYes
+    .setColor("#831fde")
+    .setTitle("Irei me lembrar!! 👌")
+    .setDescription(
+      `**O aniversário de ${u.username}#${u.discriminator} será em date!**`
+    );
+  confirmNo
+    .setColor("#831fde")
+    .setTitle("Se está com problemas, a sintaxe correta é:")
+    .setDescription(`**${prefix}set bd <mention> <mes/dia>**`);
 
   const handleReactions = (reaction, user) => {
     const emoji = reaction._emoji.name;
@@ -32,27 +41,18 @@ module.exports = async (message, uID, date) => {
     }
   };
 
-  // var msgEmbed = await generateEmbed(message, args, ID);
-
   client.on("messageReactionAdd", async (reaction, user) => {
     if (user.id == "805035898990755850") return;
-
     switch (handleReactions(reaction, user)) {
       case true:
-        confirmYes
-          .setColor("#831fde")
-          .setTitle("Irei me lembrar!! 👌")
-          .setDescription(
-            `**O aniversário de ${u.username}#${u.discriminator} será em date!**`
-          );
-        await message.channel.send(confirmYes);
+        for (const wMessage in message) {
+          await wMessage.edit(confirmYes);
+        }
         return;
       case false:
-        confirmNo
-          .setColor("#831fde")
-          .setTitle("Se está com problemas, a sintaxe correta é:")
-          .setDescription(`**${prefix}set bd <mention> <mes/dia>**`);
-        await message.channel.send(confirmNo);
+        for (const wMessage in message) {
+          await wMessage.edit(confirmNo);
+        }
         return;
     }
   });
--- util/addReaction.js
+++ util/addReaction.js
@@ -2,7 +2,7 @@ const addReaction = (message, reactions) => {
   message.react(reactions[0]);
   reactions.shift();
   if (reactions.length > 0) {
-    setTimeout(() => addReaction(message, reactions), 500);
+    setTimeout(() => addReaction(message, reactions), 750);
   }
 };
 
