069620dfdf36f44cb02dfde2cfa54c70d6dfe924
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-16 15:48:20 -0300

    Update update.js

--- util/update.js
+++ util/update.js
@@ -1,11 +1,11 @@
 // Update MongoDB on "util/update.js".
 const mongo = require("../mongo");
-const remindSchema = require("../schemes/main-schema");
+const BDStorage = require("../schemes/main-schema");
 
 module.exports = async (date, u) => {
   await mongo().then(async (mongoose) => {
     try {
-      await remindSchema
+      await BDStorage
         .findOneAndUpdate(
           {
             _id: u,
