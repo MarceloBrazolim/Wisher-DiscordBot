87baea3be8587e5dc8b7dea5c0f7dbf6bdf7089d
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-16 20:12:09 -0300

    Update listBirthday.js

--- commands/listBirthday.js
+++ commands/listBirthday.js
@@ -5,21 +5,22 @@ const moment = require("moment");
 
 module.exports = async (message, u, client) => {
   var ID = client.users.cache.get("805035898990755850");
+  moment.locale("pt-br");
   await mongo().then(async (mongoose) => {
     try {
       await BDStorage.findOne({
         _id: u.id,
       }).then((results) => {
         console.log(results);
-        
-        const { bdate } = results.bddates;
+
+        // const { bdate } = results.bddates;
         const listEmbed = new Discord.MessageEmbed()
           .setColor("#831fde")
           .setTitle("Aniversariante")
           .setAuthor("Wisher", ID.displayAvatarURL({ dynamic: true }))
           .addField(
             `@${u.username} faz aniversário em`,
-            `${moment(new Date(bdate)).format("DD [de] MMMM")}`
+            `${moment(new Date(results.bdate)).format("DD [de] MMMM")}`
           );
 
         message.channel.send(listEmbed);
