2370e43f61642102e5f1b02faefd581333c2d0bb
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-16 14:59:15 -0300

    Update main-schema.js

--- schemes/main-schema.js
+++ schemes/main-schema.js
@@ -1,16 +1,19 @@
 // Schema on "schemes/main-schema.js".
 const mongoose = require("mongoose");
-const reminder = mongoose.Schema({
-  //member ID
-  _id: {
-    type: String,
-    required: true,
+const reminder = mongoose.Schema(
+  {
+    //member ID
+    _id: {
+      type: String,
+      required: true,
+    },
+    //Birthday Date
+    Date: {
+      type: String,
+      required: true,
+    },
   },
-  //Birthday Date
-  Date: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("wisherReminder", reminder);
