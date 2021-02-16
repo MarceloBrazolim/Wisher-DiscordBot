752b879513cbf906064b02a8875453de7fcef7ff
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-16 17:28:22 -0300

    Update listBirthday.js

--- commands/listBirthday.js
+++ commands/listBirthday.js
@@ -1,5 +1,24 @@
+const Discord = require("discord.js");
 const mongo = require("../mongo");
 const BDStorage = require("../schemes/main-schema");
+const moment = require("moment");
 
 module.exports = async (message, args, u, client) => {
+  const id = u.id;
+  await mongo().then(async (mongoose) => {
+    try {
+      const results = await BDStorage.findOne({
+        id,
+      });
+
+      const listEmbed = new Discord.MessageEmbed()
+        .setColor("#831fde")
+        .setTitle("Lista de comandos disponíveis")
+        .setAuthor("Wisher", ID.displayAvatarURL({ dynamic: true }))
+        .addField();
+      message.channel.send(listEmbed)
+    } finally {
+      mongoose.connection.close();
+    }
+  });
 };
