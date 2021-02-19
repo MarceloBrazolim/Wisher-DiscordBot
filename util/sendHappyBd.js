85fae4803a927b19edafb8806cfa4ea5484e39bb
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-18 21:52:40 -0300

    Update sendHappyBd.js

--- util/sendHappyBd.js
+++ util/sendHappyBd.js
@@ -1,8 +1,29 @@
+const Discord = require("discord.js");
 const getImage = require("../../util/getImage");
+const getUserID = require("./getUserID");
 
 module.exports = async (message) => {
+  // Get gif
   const xpath = ".GifList .column .GifListItem .Gif img";
-  const path = "https://tenor.com/search/astolfo-gifs";
+  const path = "https://tenor.com/search/celebration-gifs";
   const att = "src";
   await message.channel.send(await getImage(message, path, xpath, att));
+
+  const user = getUserID(message);
+
+  // Embed
+  var birthdayEmbed = new Discord.MessageEmbed()
+    .setColor("#831fde")
+    .setTitle("Hoje é seu aniversário!")
+    .setAuthor(user.username, user.displayAvatarURL({ dynamic: true }))
+    .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+    .addField(
+      "Obrigada por fazer parte do meu server!",
+      "Muitas felicidades e muitos anos de vida pra você,\ntenha um ótimo dia e um excelente aniversário!",
+      true
+    )
+    .setImage(urls[0])
+    .addField("🇭 🇦 🇵 🇵 🇾 🥳 🎉 👏\n🇧 🇮 🇷 🇹 🇭 🇩 🇦 🇾");
+
+  message.channel.send(birthdayEmbed);
 };
