e76c91d34b43f653e2c192b2da01f40bf6a444fa
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-22 11:11:55 -0300

    Update checkBirthday.js

--- commands/checkBirthday.js
+++ commands/checkBirthday.js
@@ -7,14 +7,14 @@ module.exports = async (message, client, args) => {
   const user = message.mentions.users.first();
   if (!args[1]) {
     console.log("X|>|No Mention or Date");
-    await message.channel.send("Não entendi.. 🧐");
+    await message.channel.send("Não entendi..");
     return;
   }
   if (!user) {
     const date = new Date(args[1]);
     if (date == "Invalid date") {
       console.log("X|>|Invalid Mention or Date");
-      await message.channel.send("Usuário ou data inválida.. 🧐");
+      await message.channel.send("Usuário ou data inválida..");
       return;
     }
   }
@@ -23,33 +23,30 @@ module.exports = async (message, client, args) => {
   moment.locale("pt-br");
   await mongo().then(async (mongoose) => {
     try {
-      await BDStorage.findOne({
+      const results = await BDStorage.findOne({
         _id: user.id,
         bdate: date,
-      }).then((results) => {
-        console.log(results);
-
-        const listEmbed = new Discord.MessageEmbed()
-          .setColor("#831fde")
-          .setTitle("Aniversariante")
-          .setAuthor("Wisher", ID.displayAvatarURL({ dynamic: true }));
-
-        if (user) {
-          listEmbed.addField(
-            `@${results._id.username} faz aniversário em`,
-            `${moment(new Date(results.bdate)).format("DD [de] MMMM")}`
-          );
-        } else if (date) {
-          listEmbed.addField(
-            `@${u.username} faz aniversário em`,
-            `${moment(new Date(results.bdate)).format("DD [de] MMMM")}`
-          );
-        } else {
-          throw "X|>|Fatal Error: unknown.";
-        }
-
-        message.channel.send(listEmbed);
       });
+      const listEmbed = new Discord.MessageEmbed()
+        .setColor("#831fde")
+        .setTitle("Aniversariante")
+        .setAuthor("Wisher", ID.displayAvatarURL({ dynamic: true }));
+
+      if (user) {
+        listEmbed.addField(
+          `@${results._id.username} faz aniversário em`,
+          `${moment(new Date(results.bdate)).format("DD [de] MMMM")}`
+        );
+      } else if (date) {
+        listEmbed.addField(
+          `@${u.username} faz aniversário em`,
+          `${moment(new Date(results.bdate)).format("DD [de] MMMM")}`
+        );
+      } else {
+        throw "X|>|Fatal Error: unknown.";
+      }
+
+      message.channel.send(listEmbed);
     } catch {
       console.error();
       await message.channel.send("Não achei registros na minha lista.. 🧐");
