b72cc1f06591ab7671f387b457db477f7f242acc
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-27 19:32:43 -0300

    Update gif.js

--- commands/imgInit/gif.js
+++ commands/imgInit/gif.js
@@ -1,11 +1,11 @@
 const getImage = require("../../util/getImage");
 
-module.exports = async (message, args) => {
+module.exports = async (message, args, command) => {
   if (args[0]) {
     const xpath = ".GifList .column .GifListItem .Gif img";
     const path = "https://tenor.com/search/" + args + "-gifs"; // gif search engine
     const att = "src";
-    await getImage(message, path, xpath, att, { option: "msg" });
+    await getImage(message, path, xpath, att, command);
   } else {
     await message.channel.send("Mas você nem me falou o que procurar!");
   }
