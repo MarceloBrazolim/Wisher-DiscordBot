ad7decbc8cc72541cdd33f79c3142f5d66c2f6d6
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-27 19:32:45 -0300

    Update astolfo.js

--- commands/imgInit/astolfo.js
+++ commands/imgInit/astolfo.js
@@ -1,9 +1,9 @@
 const getImage = require("../../util/getImage");
 
-module.exports = async (message) => {
+module.exports = async (message, command) => {
   const xpath = ".GifList .column .GifListItem .Gif img";
   const path = "https://tenor.com/search/astolfo-gifs";
   const att = "src";
-  await getImage(message, path, xpath, att, { option: "msg" });
+  await getImage(message, path, xpath, att, command);
   return;
 };
