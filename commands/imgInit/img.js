d59bb65e6b773ec878c48fcad02ed1da4bbead45
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-27 19:32:41 -0300

    Update img.js

--- commands/imgInit/img.js
+++ commands/imgInit/img.js
@@ -1,11 +1,11 @@
 const getImage = require("../../util/getImage");
 
-module.exports = async (message, args) => {
+module.exports = async (message, args, command) => {
   if (args[0]) {
     const xpath = ".image a.link";
     const path = "https://www.dogpile.com/serp?qc=images&q=" + args; // jpg/png search engine
     const att = "href";
-    await getImage(message, path, xpath, att, { option: "msg" });
+    await getImage(message, path, xpath, att, command);
   } else {
     await message.channel.send("Mas você nem me falou o que procurar!");
   }
