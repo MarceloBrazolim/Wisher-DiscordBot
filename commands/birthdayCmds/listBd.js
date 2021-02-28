61379932420ba4e6da61e2b650cee52d06fd2307
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-28 11:55:41 -0300

    Update listBd.js

---
+++ commands/birthdayCmds/listBd.js
@@ -0,0 +1,23 @@
+// const Discord = require("discord.js");
+const mongo = require("../../mongo");
+const BDStorage = require("../../schemes/main-schema");
+// const moment = require("moment");
+
+module.exports = async (message) => {
+  await mongo().then(async (mongoose) => {
+    try {
+      const results = await BDStorage.findOne({
+        _id: user.id,
+      });
+      if (!results) {
+        await message.channel.send("Não achei registros na minha lista.. 🧐");
+        return;
+      }
+    } finally {
+      await mongoose.connection.close();
+      return;
+    }
+  });
+
+  return;
+};
