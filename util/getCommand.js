1cd47f12f0f088714c54a1f408b0367bfe5b56d2
MarceloMarchezini <tchetchelo01@gmail.com>
2021-02-28 13:47:13 -0300

    Update getCommand.js

--- util/getCommand.js
+++ util/getCommand.js
@@ -2,11 +2,7 @@ const runCommand = require("./runCommand");
 
 // method that defines how the commands interacts
 module.exports = async (message, command, args, client) => {
-  try {
-    await runCommand(message, command, args, client);
-  } catch {
-    console.error(`X|^|${runCommand}`);
-  } finally {
-    return;
-  }
+  await runCommand(message, command, args, client);
+  // console.error(`X|^|Err on runCommand.js`);
+  return;
 };
