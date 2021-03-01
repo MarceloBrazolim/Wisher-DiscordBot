e9ab5c49519308b8c352f3625ffbeb64b16166fe
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-01 20:22:46 -0300

    Update checkBdDates.js

--- commands/birthdayCmds/checkBdDates.js
+++ commands/birthdayCmds/checkBdDates.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const mongo = require("../../mongo");
 const BDStorage = require("../../schemes/main-schema");
 // const moment = require("moment");
+const getUserID = require("../../util/getUserID");
+const getImage = require("../../util/getImage");
 const isDateToday = require("../../util/isDateToday");
 
 module.exports = async (message) => {
@@ -13,30 +15,28 @@ module.exports = async (message) => {
         return;
       }
 
-      var todayBdEmbed = new Discord.MessageEmbed()
-        .setAuthor()
-        .setDescription();
-          // Send gif
-  const xpath = ".GifList .column .GifListItem .Gif img";
-  const path = "https://tenor.com/search/celebration-gifs";
-  const att = "src";
+      // Send gif
+      const xpath = ".GifList .column .GifListItem .Gif img";
+      const path = "https://tenor.com/search/celebration-gifs";
+      const att = "src";
 
-  const user = await getUserID(message, command);
-  // Embed
-  const todayBdEmbed = new Discord.MessageEmbed()
-    .setColor("#831fde")
-    .setTitle("Hoje é seu aniversário!")
-    // .setAuthor(user.username, user.displayAvatarURL({ dynamic: true }))
-    .setDescription(`<@!${user.id}>`)
-    .setThumbnail(user.displayAvatarURL({ dynamic: true }))
-    .addField(
-      "Obrigada por fazer parte do nosso server!",
-      "Muitas felicidades e muitos anos de vida pra você,\ntenha um ótimo dia e um excelente aniversário!",
-      true
-    )
-    .addField("🇭 🇦 🇵 🇵 🇾 🥳 🎉 👏", "🇧 🇮 🇷 🇹 🇭 🇩 🇦 🇾")
-    .setTimestamp();
-  await getImage(message, path, xpath, att, command, todayBdEmbed);
+      const user = await getUserID(message, command);
+
+      // Embed
+      const todayBdEmbed = new Discord.MessageEmbed()
+        .setColor("#831fde")
+        .setTitle("Hoje é seu aniversário!")
+        // .setAuthor(user.username, user.displayAvatarURL({ dynamic: true }))
+        .setDescription(`<@!${user.id}>`)
+        .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+        .addField(
+          "Obrigada por fazer parte do nosso server!",
+          "Muitas felicidades e muitos anos de vida pra você,\ntenha um ótimo dia e um excelente aniversário!",
+          true
+        )
+        .addField("🇭 🇦 🇵 🇵 🇾 🥳 🎉 👏", "🇧 🇮 🇷 🇹 🇭 🇩 🇦 🇾")
+        .setTimestamp();
+      await getImage(message, path, xpath, att, command, todayBdEmbed);
 
       for (let date of results) {
         if (await isDateToday(date.bdate)) {
