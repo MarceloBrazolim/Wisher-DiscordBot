8a2f5716399b9dc7f02fea19868b0c968190028b
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-01 18:13:59 -0300

    Update checkBdDates.js

--- commands/birthdayCmds/checkBdDates.js
+++ commands/birthdayCmds/checkBdDates.js
@@ -2,25 +2,24 @@
 const mongo = require("../../mongo");
 const BDStorage = require("../../schemes/main-schema");
 // const moment = require("moment");
-
 const isDateToday = require("../../util/isDateToday");
 
-module.exports = (dates) => {
-    await mongo().then(async (mongoose) => {
-        try {
-          const results = await BDStorage.find();
-          if (!results) {
-            await message.channel.send("Não achei registros na minha lista.. 🧐");
-            return;
-          }
-        } finally {
-          await mongoose.connection.close();
-          return;
-        }
-      });
-
-      for (let date of dates) {
-          isDateToday(date);
-        }
+module.exports = async (message, dates) => {
+  await mongo().then(async (mongoose) => {
+    try {
+      const results = await BDStorage.find();
+      if (!results) {
+        await message.channel.send("Não achei registros na minha lista.. 🧐");
         return;
+      }
+
+      for (let date of results) {
+        isDateToday(date.bdate);
+      }
+      return;
+    } finally {
+      await mongoose.connection.close();
+      return;
+    }
+  });
 };
