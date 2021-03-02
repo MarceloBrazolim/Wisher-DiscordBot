2e5f3b6fb42806bf986469bed44e2a99ac21556b
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-02 09:39:19 -0300

    Update bdInit.js

--- commands/bdInit.js
+++ commands/bdInit.js
@@ -1,6 +1,5 @@
 const setBirthday = require("./birthdayCmds/setBirthday");
 const checkBirthday = require("./birthdayCmds/checkBirthday");
-const sendHappyBd = require("./birthdayCmds/sendHappyBd");
 const listBd = require("./birthdayCmds/listBd");
 const checkBdDates = require("./birthdayCmds/checkBdDates");
 
@@ -8,10 +7,6 @@ module.exports = async (message, args, command, client) => {
   switch (args[0]) {
     case "set":
       await setBirthday(message, args);
-      break;
-    case "sendH":
-    case "h":
-      await sendHappyBd(message, command);
       break;
     case "list":
     case "l":
