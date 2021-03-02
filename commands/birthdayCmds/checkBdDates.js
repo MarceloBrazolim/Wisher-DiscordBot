89fca50a53d36d047eb9beac19b5937362bfebe9
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-02 07:10:00 -0300

    Update checkBdDates.js

--- commands/birthdayCmds/checkBdDates.js
+++ commands/birthdayCmds/checkBdDates.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const mongo = require("../../mongo");
 const BDStorage = require("../../schemes/main-schema");
 // const moment = require("moment");
-const getUserID = require("../../util/getUserID");
+// const getUserID = require("../../util/getUserID");
 const getImage = require("../../util/getImage");
 const isDateToday = require("../../util/isDateToday");
 
@@ -14,8 +14,34 @@ module.exports = async (message, command, client) => {
         await message.channel.send("Não achei registros na minha lista.. 🧐");
         return;
       }
+      console.log(results);
+
+      if (!results[1]) {
+        // Send gif
+        const xpath = ".GifList .column .GifListItem .Gif img";
+        const path = "https://tenor.com/search/celebration-gifs";
+        const att = "src";
+        // Embed
+        const user = client.users.cache.get(results[0]._id);
+        const birthdayEmbed = new Discord.MessageEmbed()
+          .setColor("#831fde")
+          .setTitle("Hoje é seu aniversário!")
+          .setDescription(`<@!${results[0]._id}>`)
+          .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+          .addField(
+            "Obrigada por fazer parte do nosso server!",
+            "Muitas felicidades e muitos anos de vida pra você,\ntenha um ótimo dia e um excelente aniversário!",
+            true
+          )
+          .addField("🇭 🇦 🇵 🇵 🇾 🥳 🎉 👏", "🇧 🇮 🇷 🇹 🇭 🇩 🇦 🇾")
+          .setTimestamp();
+        await getImage(message, path, xpath, att, command, birthdayEmbed);
+        return;
+      }
 
       for (let result of results) {
+        if (!isDateToday(result.bdate)) continue;
+
         // Send gif
         const xpath = ".GifList .column .GifListItem .Gif img";
         const path = "https://tenor.com/search/celebration-gifs";
