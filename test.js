a098f6f9ead861af71d864c416f633177769318a
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-02 07:46:43 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
+const moment = require("moment");
+
 const bd = new Date("7-25");
 var now = new Date();
 
-if (bd.getMonth() === now.getMonth() && bd.getDate() === now.getDate()) {
-  console.log("true");
-} else {
-  console.log("false");
-}
+moment.locale("pt-br");
 
-console.log(bd.getMonth());
+console.log(
+  moment()
+    .month(new Date().getMonth())
+    .date(new Date().getDate())
+    .format("DD [de] MMMM [de] YYYY")
+);
+
+// if (bd.getMonth() === now.getMonth() && bd.getDate() === now.getDate()) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// console.log(bd.getMonth());
