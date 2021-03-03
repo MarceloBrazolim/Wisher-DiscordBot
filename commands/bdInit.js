97d726b0909e4cfe18d35caee3ba1b1405d1c6e3
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-03 19:45:35 -0300

    Update bdInit.js

--- commands/bdInit.js
+++ commands/bdInit.js
@@ -2,11 +2,21 @@ const setBirthday = require("./birthdayCmds/setBirthday");
 const checkBirthday = require("./birthdayCmds/checkBirthday");
 const listBd = require("./birthdayCmds/listBd");
 const checkBdDates = require("./birthdayCmds/checkBdDates");
+const del = require("../util/delete");
+
+const isAdm = require("../util/isAdm");
+const getUserID = require("../util/getUserID");
 
 module.exports = async (message, args, command, client) => {
+  args[0] = args[0].toLowerCase();
   switch (args[0]) {
     case "set":
-      await setBirthday(message, args);
+      let uid = await getUserID(message);
+      console.log(`${message.author.id}\n${uid.id}`);
+      if (message.author.id != uid.id) {
+        if (!(await isAdm(message))) break;
+      }
+      await setBirthday(message, args, uid);
       break;
     case "list":
     case "l":
@@ -15,11 +25,20 @@ module.exports = async (message, args, command, client) => {
     case "check":
       await checkBirthday(message);
       break;
-    case "anyToday":
+    case "anytoday":
     case "today":
-    case "bdToday":
-    case "birthdayToday":
+    case "bdtoday":
+    case "birthdaytoday":
       await checkBdDates(message, command, client);
+      break;
+    case "delete":
+    case "del":
+      let u = await getUserID(message);
+      console.log(`${message.author.id}\n${u.id}`);
+      if (message.author.id != u.id) {
+        if (!(await isAdm(message))) break;
+      }
+      await del(u.id, message);
       break;
   }
   return;
