30ea4a1ea51a10f288def3606ed30fe85a4599d4
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-03 19:45:12 -0300

    Update isAdm.js

--- util/isAdm.js
+++ util/isAdm.js
@@ -2,10 +2,11 @@
 
 module.exports = async (message) => {
   if (message.guild.member(message.author).hasPermission("ADMINISTRATOR")) {
-    await message.react("👍");
+    // await message.react("👍");
     return true;
   } else {
-    console.log("x|>|Err: User without permission to perform action.")
-    await message.channel.send("Você não tem permissão pra fazer isso.");}
+    console.log("x|>|Err: User without permission to perform action.");
+    await message.channel.send("Você não tem permissão pra fazer isso.");
+  }
   return false;
 };
