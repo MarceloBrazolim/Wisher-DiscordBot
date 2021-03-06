f95c47a730a35182c851fa308e6cc67aa378f811
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-05 21:30:06 -0300

    Update listBd.js

--- commands/birthdayCmds/listBd.js
+++ commands/birthdayCmds/listBd.js
@@ -3,39 +3,34 @@ const mongo = require("../../mongo");
 const BDStorage = require("../../schemes/main-schema");
 const moment = require("moment");
 
-module.exports = async (message, client) => {
+module.exports = async (message) => {
   moment.locale("pt-br");
-  await mongo().then(async (mongoose) => {
+  await mongo().then(async () => {
     try {
       // const guildID = message.channel.guild.id;
       const results = await BDStorage.find({
-        gID: message.channel.guild.id,
+        gID: { $in: [message.channel.guild.id] },
       });
       if (!results[0]) {
         await message.channel.send("Não achei registros na minha lista.. 🧐");
         throw "No registry";
       }
 
+      let userList = "";
+      for (let result of results) {
+        userList += `- <@!${result.mID}> faz aniversário em **${moment(
+          new Date(result.bdate)
+        ).format("D [de] MMMM")}**\n`;
+      }
+      console.log(userList);
       const listEmbed = new Discord.MessageEmbed()
         .setColor("#831fde")
-        .setTitle("Aniversariantes");
-
-      for (let result of results) {
-        let tmpUser = client.users.cache.get(result.mID);
-        listEmbed.addField(
-          `${tmpUser.username}#${tmpUser.discriminator}`,
-          `faz aniversário em **${moment(new Date(result.bdate)).format(
-            "D [de] MMMM"
-          )}**`
-        );
-      }
+        .setTitle("Aniversariantes")
+        .setDescription(`${userList}`);
 
       await message.channel.send(listEmbed);
-    } catch (e) {
-      console.log(`X|>|${e} at listBd.js`);
-    } finally {
-      await mongoose.connection.close();
-      return;
+    } catch (err) {
+      console.log(`X|>|${err} at listBd.js`);
     }
   });
 
