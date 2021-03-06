1a0f69922dac35664f76af98c7d3b9a1dd3c4947
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-06 01:49:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 const mongo = require("./mongo");
+const addWhenJoin = require("./util/addWhenJoin");
+const delWhenLeave = require("./util/delWhenLeave");
 
 const config = require("./config.json");
 const { name, version } = require("./package.json");
@@ -36,8 +39,21 @@ client.once("ready", async () => {
         "MENTION_EVERYONE",
       ],
     })
-    .then((link) => console.log(`||>|Bot invite link: ${link}`))
+    .then((link) => {
+      console.log(`||>|Bot invite link: ${link}`);
+    })
     .catch(console.error);
+});
+
+// Triggers everytime bot joins a guild
+client.on("guildCreate", async function (guild) {
+  console.log(`||>|Bot Joined guild: ${guild.id}`);
+  await addWhenJoin(guild.id);
+});
+// Trigger whenever a guild is deleted/left
+client.on("guildDelete", async function (guild) {
+  console.log(`||>|Bot left guild: ${guild.id}`);
+  await delWhenLeave(guild.id);
 });
 
 client.on("message", async (message) => {
@@ -55,7 +71,7 @@ client.on("message", async (message) => {
     .split("%20");
   const command = args.shift().toLowerCase();
   console.log(
-    `||_:\n||=|${message.author.username}#${message.author.discriminator}:\n||<|Command: { ${config.prefix}${command} ${args} }`
+    `||_:\n||=|${message.author.username}#${message.author.discriminator}:\n||<|Command: { ${config.prefix}${command}: ${args} }`
   );
 
   await getCommand(message, command, args, client);
