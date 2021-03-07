42c4f8a26e3bee107726f913ff48497dfd5e3f04
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-07 15:51:58 -0300

    Update update.js

--- util/update.js
+++ util/update.js
@@ -10,7 +10,7 @@ module.exports = async (guildID, date, u) => {
           _id: u,
         },
         {
-          $push: { gID: guildID },
+          $addToSet: { gID: guildID },
           bdate: date,
         },
         {
