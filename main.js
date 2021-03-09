4184d8bb05ca13f77dd76b610c0144ff0778352e
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-08 21:30:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -58,11 +58,11 @@ client.on("guildDelete", async function (guild) {
   await deleteGuild(guild.id);
 });
 
-// client.on("disconnect", async function (event) {
-//   await mongo().then((mongoose) => {
-//     mongoose.connection.close();
-//   });
-// });
+client.on("disconnect", async function (event) {
+  await mongo().then((mongoose) => {
+    mongoose.connection.close();
+  });
+});
 
 client.on("message", async (message) => {
   if (
