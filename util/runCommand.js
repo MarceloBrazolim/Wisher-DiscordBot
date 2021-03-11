dea01b0e21036bf15014fc194d9ee50af93bc4ca
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-11 16:32:49 -0300

    Update runCommand.js

--- util/runCommand.js
+++ util/runCommand.js
@@ -84,9 +84,9 @@ module.exports = async (message, command, args, client) => {
         "HELLO. WOULD YOU LIKE TO SIGN MY PETITION? 📑"
       );
       break;
-    case "bundinha":
-      await message.channel.send("Se dá depois das 6 da matina?");
-      break;
+    // case "bundinha":
+    //   await message.channel.send("Se dá depois das 6 da matina?");
+    //   break;
   }
   return;
 };
