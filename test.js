e611180e341622201e49c63bbc1f00a5cf8aced9
MarceloMarchezini <tchetchelo01@gmail.com>
2021-03-13 21:22:21 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,19 @@
 const moment = require("moment");
 
-const dates = ["7-25", "5-5", "3-13"];
+const dates = ["7-25", "3-13", "5-5"];
+const sortedDates = dates.sort(
+  (a, b) =>
+    new moment(new Date(a)).format("MMDD") -
+    new moment(new Date(b)).format("MMDD")
+);
+console.log(sortedDates);
 
 const todayYear = new Date().getFullYear();
 
-for (let date of dates) {
+for (let date of sortedDates) {
   let test = moment(new Date()).isBefore(new Date(date).setFullYear(todayYear));
   console.log(
-    moment(new Date(date).setFullYear(todayYear)).format("[-] D[/]M[/]YYYY"),
+    moment(new Date(date).setFullYear(todayYear)).format("DD[/]MM[/]YYYY [-]"),
     test
   );
 }
