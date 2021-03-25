4ab993ed3c85e432204ac80d3e8e546a826028b8
MarceloBrazolim <marcelobrazolim@gmail.com>
2021-03-25 16:50:55 -0300

    Update astolfo.js

--- commands/imgInit/astolfo.js
+++ commands/imgInit/astolfo.js
@@ -1,23 +1,23 @@
-// const getImage = require("../../util/getImage");
+const getImage = require("../../util/getImage");
 
-// module.exports = async (message, command) => {
-//   const xpath = ".GifList .column .GifListItem .Gif img";
-//   const path = "https://tenor.com/search/astolfo-gifs";
-//   const att = "src";
-//   await getImage(message, path, xpath, att, command);
-//   return;
-// };
-
-const { shuffle } = require("lodash");
-
-module.exports = async (message, google) => {
-  const result = shuffle(await google.scrape("astolfo+fate+apocrypha+gif", 20));
-  // const result = shuffle(await google.scrape(query + "&safe=active", 40));
-  if (result[0] === undefined) {
-    message.channel.send("Ops.. Não achei o que procurava.. 😔");
-    throw "No results found";
-  }
-  console.log(`||>|Url: ${result[0].url}`);
-
-  return result[0];
+module.exports = async (message) => {
+  const xpath = ".GifList .column .GifListItem .Gif img";
+  const path = "https://tenor.com/search/astolfo-gifs";
+  const att = "src";
+  await getImage(message, path, xpath, att);
+  return;
 };
+
+// const { shuffle } = require("lodash");
+
+// module.exports = async (message, google) => {
+//   const result = shuffle(await google.scrape("astolfo+fate+apocrypha+gif", 20));
+//   // const result = shuffle(await google.scrape(query + "&safe=active", 40));
+//   if (result[0] === undefined) {
+//     message.channel.send("Ops.. Não achei o que procurava.. 😔");
+//     throw "No results found";
+//   }
+//   console.log(`||>|Url: ${result[0].url}`);
+
+//   return result[0];
+// };
